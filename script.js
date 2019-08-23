880b6c8e0e52358cf6b289bb756a496bf115460d
aishakhan267 <49521671+aishakhan267@users.noreply.github.com>
2019-08-24 03:42:50 +0500

    task added stopwatch

---
+++ script.js
@@ -0,0 +1,48 @@
+let sound = new Audio("click.mp3");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+let isRunning = false;
+function updateTime() {
+    if (seconds === 59) {
+        seconds = 0;
+        if (minutes === 59) {
+            minutes = 0;
+            hours++;
+        } else {
+            minutes++;
+        }
+    } else {
+        seconds++;
+    }
+
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+}
+function start() {
+    if (isRunning === false) {
+        sound.play();
+        interval = setInterval(updateTime, 1000);
+        isRunning = true;
+    }
+}
+function stop() {
+    sound.play();
+
+    clearInterval(interval);
+    isRunning = false;
+}
+function reset() {
+    sound.play();
+
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    stop();
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+} 
